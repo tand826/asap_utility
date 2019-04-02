617a2a484e238836ec8a4f962372f203c8d42f7f
takumiando <tando@metadata.co.jp>
2019-04-02 18:29:00 +0900

    support rects

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # asap_utility
 Utility script for ASAP.
+
+# TODO
+- Type "Rect", "Non Rect"
--- converter.py
+++ converter.py
@@ -141,8 +141,7 @@ class MaskMaker(Setup):
     def make_mask(self):
         # Make base masks.
         self.masks = {i: np.zeros((self.height, self.width), dtype=np.uint8) for i in self.classes}
-        for annotation in self.annotations:
-            # anno_type = annotation.attrib["Type"]  # for applying rules.
+        for annotation in self.annotations:  # for applying rules.
             group = annotation.attrib["PartOfGroup"]  # for applying rules.
             if group in self.classes:
                 contour = []
@@ -193,11 +192,35 @@ class PatchMaker(MaskMaker):
     def make_patch_parallel(self):
         print(f"Processing {self.tiles[0]*self.tiles[1]} patches x {len(self.classes)} classes.")
         for group in self.classes:
-            if np.sum(self.masks[group]) > 0:
-                print(f"{'-'*10} {group} {'-'*10}")
-                iterator = product(list(range(self.tiles[0])), list(range(self.tiles[1])))
-                parallel = Parallel(n_jobs=-1, verbose=1, backend="threading")
-                parallel([delayed(self.make_patch)(x, y, group) for x, y in iterator])
+            if self.args.mode == "non_rect" or self.args.mode == "all":
+                if np.sum(self.masks[group]) > 0:
+                    print(f"{'-'*10} {group} {'-'*10}")
+                    iterator = product(list(range(self.tiles[0])), list(range(self.tiles[1])))
+                    parallel = Parallel(n_jobs=-1, verbose=1, backend="threading")
+                    parallel([delayed(self.make_patch)(x, y, group) for x, y in iterator])
+            elif self.args.mode == "rect":
+                print("here")
+                self.make_rect()
+
+    def make_rect(self):
+        for annotation in self.annotations:
+            shapetype = annotation.attrib["Type"]
+            group = annotation.attrib["PartOfGroup"]
+            save_to = Path(f"{self.outdir}/{group}_rect")
+            self._check(save_to)
+            if group in self.classes and shapetype == "Rectangle":
+                contour = []
+                for point in annotation.xpath("Coordinates/Coordinate"):
+                    x = np.int32(np.float(point.attrib["X"]))
+                    y = np.int32(np.float(point.attrib["Y"]))
+                    contour.append([x, y])
+                contour = np.array(contour)
+                minx = np.min(contour[:, 0])
+                miny = np.min(contour[:, 1])
+                maxx = np.max(contour[:, 0])
+                maxy = np.max(contour[:, 1])
+                rect = self.img.read_region((minx, miny), 0, (maxx - minx, maxy - miny))
+                rect.save(str(save_to/f"{minx}_{miny}_{maxx}_{maxy}".png))
 
     def make_patch(self, x, y, group):
         if self.is_onshore(x, y, group):
